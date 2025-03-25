d832a36476aff990322f5b336a5265a26daed595
Aaron Marburg <amarburg@uw.edu>
2025-03-25 12:15:05 -0700

    Add Nvidia key

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
-echo "deb https://repo.download.nvidia.com/jetson/common <release> main" > /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
-echo "deb https://repo.download.nvidia.com/jetson/<platform> <release> main" >> /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
+apt-key adv --fetch-key https://repo.download.nvidia.com/jetson/jetson-ota-public.asc
+echo "deb https://repo.download.nvidia.com/jetson/common r36.3 main" > /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
 
 apt-get update
 apt-get install -y \
