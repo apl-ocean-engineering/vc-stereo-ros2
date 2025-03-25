cf5996bd2ba374e907c318e7554a78fc33f032e0
Aaron Marburg <amarburg@uw.edu>
2025-03-25 12:24:13 -0700

    Add t234 repo from Nvidia

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -2,6 +2,8 @@
 
 apt-key adv --fetch-key https://repo.download.nvidia.com/jetson/jetson-ota-public.asc
 echo "deb https://repo.download.nvidia.com/jetson/common r36.3 main" > /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
+echo "deb https://repo.download.nvidia.com/jetson/t234 r36.3 main" >> /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
+
 
 apt-get update
 apt-get install -y \
